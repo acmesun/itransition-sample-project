6bf9ce7359fa557f1c56adcc7f1261766a59c32c
acmesun <danfyou@gmail.com>
2021-04-20 22:01:34 +0300

    Initial commit

---
+++ src/main/java/by/lukyanets/acmesun/AcmesunApplication.java
@@ -0,0 +1,13 @@
+package by.lukyanets.acmesun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmesunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmesunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/lukyanets/acmesun/AcmesunApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lukyanets.acmesun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmesunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
